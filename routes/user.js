a4b5c0cc52536ad72955a1e48b5d2b72dd332675
myconpeter <79177019+myconpeter@users.noreply.github.com>
2023-09-13 10:43:38 +0100

    added small stat

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,36 @@ const router = express.Router();
 const User = require('../models/user');
 const {ensureAuthenticated} = require('../config/auth');
 
-router.get("/users", ensureAuthenticated, (req, res)=>{
-    // find the campground with tihe id
-    User.find({}).exec((err, allusers) =>{
-        if (err){
-                console.log(err);
-                    } else {
-                    res.render("user", {user: allusers});
-                    }
+
+
+router.get("/users", ensureAuthenticated, (req, res) => {
+    // Count the total number of users
+    User.countDocuments({}, (err, userCount) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // Count the number of users who have invested
+            User.countDocuments({ hasInvested: true }, (err, investedUserCount) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    // Find all users
+                    User.find({}, (err, allusers) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.render("user", {
+                                userCount: userCount,
+                                investedUserCount: investedUserCount,
+                                user: allusers
+                            });
+                        }
+                    });
+                }
+            });
+        }
     });
 });
+
 
 module.exports = router; 
